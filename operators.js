890775c3706581995f60a7b6189b5bd5f95c4fe9
KritikaRaj16 <rajkritika1609@gmail.com>
2026-02-20 00:36:05 +0530

    Operators

---
+++ operators.js
@@ -0,0 +1,13 @@
+let a = 5
+let b = 6
+console.log("Sum =", a+b)
+console.log("Product =", a*b)
+console.log("Difference =", a-b)
+console.log("Division =", a/b)
+console.log("Modulus =", a%b)
+console.log("Equal or not=", 10 == "10")
+console.log("Equal or not=", 10 === '10')
+console.log(a && b)
+console.log(a || b)
+console.log(b && a)
+console.log('Gunnu' && 'Anku')
