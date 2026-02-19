d5274e45d4184362bc02ab56b86503797305e32c
KritikaRaj16 <rajkritika1609@gmail.com>
2026-02-20 00:36:43 +0530

    DataTypes

---
+++ datatypes.js
@@ -0,0 +1,16 @@
+console.log(typeof 20)
+console.log(typeof undefined)
+console.log(typeof true)
+console.log(typeof 20.5)
+console.log(typeof "Hello")
+console.log(typeof null)
+let product_name = "Chocolate ice-cream"
+let price = 5000
+let in_stock = true
+let discount
+let discount2 = null
+console.log(typeof product_name)
+console.log(typeof price)
+console.log(typeof in_stock)
+console.log(typeof discount)
+console.log(typeof discount2)
