56ab0a1dc6cb06f1e4772290a2edd912861e05bd
KritikaRaj16 <rajkritika1609@gmail.com>
2026-03-07 22:00:28 +0530

    Switch Statements

---
+++ switch.js
@@ -0,0 +1,27 @@
+let day = 9
+switch(day) {
+    case 1:
+        console.log("Monday");
+        break;
+      case 2:
+        console.log("Tuseday");
+        break;  
+      case 3:
+        console.log("Wednesday");
+        break;  
+      case 4:
+        console.log("Thursday");
+        break;  
+      case 5:
+        console.log("Friday");
+        break;  
+      case 6:
+        console.log("Khushi s jeene wla pehla din.");
+        break;  
+      case 7:
+        console.log("Khushi s jeene wla akhiri din.");
+        break;  
+        default:
+            console.log("Enter a valid day number.")
+            co
+}
