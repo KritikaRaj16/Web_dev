d710069bcda590e0dd43cdcadc105caa56e4aff4
KritikaRaj16 <rajkritika1609@gmail.com>
2026-03-07 21:57:10 +0530

    Conditons

---
+++ condition.js
@@ -0,0 +1,13 @@
+let age = 25
+isStudent = true 
+
+if (age < 18){
+    console.log("Minor")
+}
+else if (age >= 18 &&age <=60 && isStudent){
+        console.log("Adult student.")        
+    
+}
+else{
+    console.log("Senior")
+}
