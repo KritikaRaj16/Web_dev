add2e99609319e521b5744ace90f45e824352d7c
KritikaRaj16 <rajkritika1609@gmail.com>
2026-03-07 21:59:31 +0530

    Concatenation and coercion

---
+++ concatenation.js
@@ -0,0 +1,18 @@
+console.log("Hello" + "World")
+console.log("Hello" + " World")
+let str1 = "Kritika"
+let str2 = "Sharma"
+console.log(str1 + " " + str2)
+
+//type coercion 
+console.log("5" - 5)    //0
+console.log("5" + 5)   //55
+console.log("Kritika" - "Raj")   
+console.log(typeof("5" + 5))   //55
+console.log(true + true)
+console.log(typeof(true + false))
+console.log(true - false)
+console.log(false - true)
+console.log(2 >12 >5)
+console.log("20" +10 + 10)   //evalutes from left to right
+console.log(20 +10 + "10")   //evalutes from left to right
