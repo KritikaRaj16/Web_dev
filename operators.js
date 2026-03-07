9802e9962c3ea2b7dd20f7bbded8e833c241da6c
KritikaRaj16 <rajkritika1609@gmail.com>
2026-03-07 22:03:11 +0530

    Added more examples

--- operators.js
+++ operators.js
@@ -1,13 +1,27 @@
 let a = 5
 let b = 6
+
+//Arithmetic
 console.log("Sum =", a+b)
 console.log("Product =", a*b)
 console.log("Difference =", a-b)
 console.log("Division =", a/b)
 console.log("Modulus =", a%b)
-console.log("Equal or not=", 10 == "10")
-console.log("Equal or not=", 10 === '10')
+
+//Comparison 
+console.log("Is equal=", 10 == "10")   //true
+console.log("Is equal=", 10 === "10")  //false
+console.log("Is not equal=", 10 != "10")  //false
+console.log("Is not equal=", 10 !== "10")  //true
+
+//Logical
 console.log(a && b)
 console.log(a || b)
 console.log(b && a)
 console.log('Gunnu' && 'Anku')
+console.log(!('Gunnu' && 'Anku'))
+console.log('Gunnu' || 'Anku')
+console.log(!('Gunnu' || 'Anku'))   //false
+ console.log(0.1 + 0.2)
+ console.log(0.1 + 0.1)
+ console.log((0.1 + 0.2).toFixed(3))
