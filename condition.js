00dcdb7ca082efe8c0013e49547b5cbea2ec94a7
KritikaRaj16 <rajkritika1609@gmail.com>
2026-03-11 23:39:11 +0530

    Added new problem

--- condition.js
+++ condition.js
@@ -11,3 +11,12 @@ else if (age >= 18 &&age <=60 && isStudent){
 else{
     console.log("Senior")
 }
+// Question2: Check wheteher the given year is leap year or not.
+let year = 2020;
+// year = prompt("Enter a year:-");
+if ((year % 4 ===0 && year % 100 != 0) || year % 400===0){
+    console.log(year + " is leap year.");
+}
+else{
+    console.log(year + " is not leap year.");
+}
